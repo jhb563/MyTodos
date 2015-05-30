adb33d240b56b3ff9043c887b064161a8bd3a412
unknown <jhbowen047@gmail.com>
2015-05-29 19:01:37 -0700

    Projects module exists and has a deadline

---
+++ app/models/project.server.model.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Project Schema
+ */
+var ProjectSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Project name',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+        deadline: {
+		type: Date,
+    		required: 'Please enter a deadline',
+    	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Project', ProjectSchema);
